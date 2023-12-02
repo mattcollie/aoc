6a81d42f60200c2dc25d37027e91c45bb0e7c961
matt <mattcollie@live.com>
2023-12-02 22:55:55 +1300

    adding day-03 in preparation

---
+++ aoc/day-03/__init__.py
@@ -0,0 +1,36 @@
+from aoc.utils import read_test_data, read_data
+
+
+def main():
+    print(f"\nTest result: {test()}\nPart One result: {part_one()}\nPart Two result: {part_two()}\n")
+
+
+def test():
+    result = None
+
+    for data in read_test_data():
+        print(f"data -> {data}")
+
+    return result
+
+
+def part_one():
+    result = None
+
+    for data in read_data():
+        print(f"data -> {data}")
+
+    return result
+
+
+def part_two():
+    result = None
+
+    for data in read_data():
+        print(f"data -> {data}")
+
+    return result
+
+
+if __name__ == '__main__':
+    main()
