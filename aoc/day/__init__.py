25ca80cae276d54571c9f3d853d10ad8ba03548b
matt <mattcollie@live.com>
2023-12-02 22:43:37 +1300

    adding day template

---
+++ aoc/day/__init__.py
@@ -0,0 +1,38 @@
+from aoc.utils import read_test_data, read_data
+
+
+def main():
+    print(f"Test result: {test()}\n")
+    print(f"Part One result: {part_one()}\n")
+    print(f"Part Two result: {part_two()}\n")
+
+
+def test():
+    result = None
+
+    for data in read_test_data():
+        print(f"data -> {data}")
+
+    return result
+
+
+def part_one():
+    result = None
+
+    for data in read_data():
+        print(f"data -> {data}")
+
+    return result
+
+
+def part_two():
+    result = None
+
+    for data in read_data():
+        print(f"data -> {data}")
+
+    return result
+
+
+if __name__ == '__main__':
+    main()
